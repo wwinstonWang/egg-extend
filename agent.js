f753c098731e66b6aa3dc278db2b1cde76e75b86
winston <a51480139@163.com>
2019-07-02 18:00:44 +0800

    修复

--- agent.js
+++ agent.js
@@ -4,45 +4,65 @@ const ip=require("./nettools").getIPAdress();
 const feign= require("./feign");
 const NacosNamingClient = require('nacos').NacosNamingClient;
 
-module.exports = async agent => {
-    const defaultConfig = {
-        /**
-         * 微服务名称
-         */
-        name: "default",
-        /**
-         * 注册中心地址
-         */
-        discovery: {
-            serverAddr: "127.0.0.1:8848",
-            namespace: "public"
-        },
-        /**
-         * 本机地址
-         */
-        local: {
-            ip: ip,
-            port: 8080
-        }
+class AgentHook {
+    constructor(agent) {
+        this.agent = agent;
     }
-    let config = agent.config.eggExtend;
-    config = Object.assign({}, defaultConfig, config)
 
-    const logger = console;
-    const client = new NacosNamingClient({
-        logger,
-        serverList: config.discovery.serverAddr,
-        namespace: config.discovery.namespace,
-    });
-    feign.client=client;
-    await client.ready();
+    async didReady() {
+        const agent=this.agent;
 
-    const directory = path.join(app.config.baseDir, 'app/feign');
-    agent.loader.loadToApp(directory, 'feign', {
-        initializer(model, opt) {
-            // 第一个参数为 export 的对象
-            // 第二个参数为一个对象，只包含当前文件的路径
-            return new model(agent, opt.path);
-        },
-    });
-};
+        const defaultConfig = {
+            /**
+             * 微服务名称
+             */
+            name: "default",
+            /**
+             * 注册中心地址
+             */
+            discovery: {
+                serverAddr: "127.0.0.1:8848",
+                namespace: "public"
+            },
+            /**
+             * 本机地址
+             */
+            local: {
+                ip: ip,
+                port: 8080
+            }
+        }
+        let config = agent.config.eggExtend;
+        config = Object.assign({}, defaultConfig, config)
+        agent.config.eggExtend=config;
+    
+        const logger = console;
+        const client = new NacosNamingClient({
+            logger,
+            serverList: config.discovery.serverAddr,
+            namespace: config.discovery.namespace,
+        });
+        feign.client=client;
+        await client.ready();
+        // registry instance
+        await client.registerInstance(config.name, config.local);
+    
+        const directory = path.join(agent.config.baseDir, 'app/feign');
+        agent.loader.loadToApp(directory, 'feign', {
+            initializer(model, opt) {
+                // 第一个参数为 export 的对象
+                // 第二个参数为一个对象，只包含当前文件的路径
+                return new model(agent, opt.path);
+            },
+        });
+    }
+
+    async beforeClose(){
+        const feign=require("egg-extend/feign");
+        const client=feign.client;
+        const bootstrap=this.agent.config.eggExtend;
+        await client.deregisterInstance(bootstrap.name,bootstrap.local);
+    }
+}
+
+module.exports=AgentHook;
--- app.js
+++ app.js
@@ -1,49 +1,67 @@
 'use strict';
-const path = require("path");
+const path=require("path");
 const ip=require("./nettools").getIPAdress();
+const feign= require("./feign");
 const NacosNamingClient = require('nacos').NacosNamingClient;
 
-module.exports = async app => {
-    const defaultConfig = {
-        /**
-         * 微服务名称
-         */
-        name: "default",
-        /**
-         * 注册中心地址
-         */
-        discovery: {
-            serverAddr: "127.0.0.1:8848",
-            namespace: "public"
-        },
-        /**
-         * 本机地址
-         */
-        local: {
-            ip: ip,
-            port: 8080
-        }
+class ApptHook {
+    constructor(app) {
+        this.app = app;
     }
-    let config = app.config.eggExtend;
-    config = Object.assign({}, defaultConfig, config)
 
-    const logger = console;
-    const client = new NacosNamingClient({
-        logger,
-        serverList: config.discovery.serverAddr,
-        namespace: config.discovery.namespace,
-    });
+    async didReady() {
+        const app=this.app;
+        const defaultConfig = {
+            /**
+             * 微服务名称
+             */
+            name: "default",
+            /**
+             * 注册中心地址
+             */
+            discovery: {
+                serverAddr: "127.0.0.1:8848",
+                namespace: "public"
+            },
+            /**
+             * 本机地址
+             */
+            local: {
+                ip: ip,
+                port: 8080
+            }
+        }
+        let config = app.config.eggExtend;
+        config = Object.assign({}, defaultConfig, config)
+        app.config.eggExtend=config;
 
-    const feign = require("./feign");
-    feign.client = client;
-    await client.ready();
+        const logger = console;
+        const client = new NacosNamingClient({
+            logger,
+            serverList: config.discovery.serverAddr,
+            namespace: config.discovery.namespace,
+        });
+        feign.client=client;
+        await client.ready();
+        // registry instance
+        await client.registerInstance(config.name, config.local);
+    
+        const directory = path.join(app.config.baseDir, 'app/feign');
+        app.loader.loadToApp(directory, 'feign', {
+            initializer(model, opt) {
+                // 第一个参数为 export 的对象
+                // 第二个参数为一个对象，只包含当前文件的路径
+                return new model(app, opt.path);
+            },
+        });
+    }
 
-    const directory = path.join(app.config.baseDir, 'app/feign');
-    app.loader.loadToApp(directory, 'feign', {
-        initializer(model, opt) {
-            // 第一个参数为 export 的对象
-            // 第二个参数为一个对象，只包含当前文件的路径
-            return new model(app, opt.path);
-        },
-    });
-};
+    async beforeClose(){
+        const feign=require("egg-extend/feign");
+        const client=feign.client;
+        const bootstrap=this.app.config.eggExtend;
+        await client.deregisterInstance(bootstrap.name,bootstrap.local);
+    }
+}
+
+module.exports=ApptHook;
