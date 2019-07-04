d5e74d516226c9b2df1a077b9d76537ac35f3b54
winston <a51480139@163.com>
2019-07-04 10:33:01 +0800

    Feign修饰类中方法第一个参数为对象时才注入负载均衡地址

--- feign.js
+++ feign.js
@@ -2,29 +2,29 @@
  * 根据负载均衡策略从服务列表中获取服务连接信息
  * @param {注册中心服务列表} serviceList 
  */
-function balanceServiceList(serviceList){
+function balanceServiceList(serviceList) {
     //总权重
-    let sumWeight=0;
-    const flatServiceList=[];
-    serviceList.forEach(service=>{
-        sumWeight+=service.weight;
-        for(let i=0;i<service.weight;i++)
+    let sumWeight = 0;
+    const flatServiceList = [];
+    serviceList.forEach(service => {
+        sumWeight += service.weight;
+        for (let i = 0; i < service.weight; i++)
             flatServiceList.push(service);
     });
     //当前权重
-    const currentWeight=Math.floor(Math.random()*sumWeight);
-    const currentService=flatServiceList[currentWeight];
+    const currentWeight = Math.floor(Math.random() * sumWeight);
+    const currentService = flatServiceList[currentWeight];
     return `http://${currentService.ip}:${currentService.port}`
 }
 
 /**
- * 装饰器，类似java中Feign. 被Feign修饰的类中，所有函数第一个参数必须为对象，并且在运行时会自动注入负载均衡地址，键为balanceUrl
+ * 装饰器，类似java中Feign. 被Feign修饰的类中. 当函数第一个参数为对象时，运行时会自动注入负载均衡地址，键为balanceUrl
  * @param {注册中心微服务名称} serviceName 
  */
 function feign(serviceName) {
-    let serviceList=undefined;
+    let serviceList = undefined;
     client.subscribe(serviceName, hosts => {
-        serviceList=hosts;
+        serviceList = hosts;
     });
     return function (target) {
         const desc = Object.getOwnPropertyDescriptors(target.prototype);
@@ -40,12 +40,12 @@ function feign(serviceName) {
                 // 重新定义类的原方法, 
                 Object.defineProperty(target.prototype, key, {
                     value(...args) {
-                        const baseUrl=balanceServiceList(serviceList);
+                        const baseUrl = balanceServiceList(serviceList);
                         console.log(`当前地址为${baseUrl}`);
-                        if(args.length>0)
-                            args[0].balanceUrl=baseUrl;
+                        if (args.length > 0 && typeof args[0] == "object")
+                            args[0].balanceUrl = baseUrl;
                         // args.unshift(baseUrl);
-                        const ret = func.apply(this,args);
+                        const ret = func.apply(this, args);
                         return ret;
                     }
                 })
@@ -60,7 +60,7 @@ module.exports = {
     /**
      * 设置nacos订阅
      */
-    set client(value){
-        client=value;
+    set client(value) {
+        client = value;
     }
 }
